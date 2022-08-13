18a643bcf92975461e3037fdc58b02038931dd75
KlingonDragon <klingondragon@outlook.com>
2022-08-13 23:13:39 +0100

    initial commit

---
+++ __main__.py
@@ -0,0 +1,21 @@
+import os, requests, time
+def pause():
+    print()
+    if os.name == 'nt':
+        os.system('pause')
+    else:
+        input('Press enter to continue...')
+def check_server():
+    try:
+        for game,data in requests.get('https://info.server.cnc-online.net/').json().items():
+            try:
+                user_count = len(data['users'])
+                if user_count:
+                    print('{}{}{:0>3} online'.format(game, (2 - (len(game) // 8)) * '\t', user_count))
+            except:
+                pass
+    except:
+        print('SERVER OFFLINE')
+    pause()
+if __name__ == '__main__':
+    check_server()
